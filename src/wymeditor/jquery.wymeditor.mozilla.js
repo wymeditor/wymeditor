67dba50cf7f6d6b84d5cd0b4e2c9f04da5b79897
Jean-Francois Hovinne <jf.hovinne@wymeditor.org>
2007-02-22 10:31:28 +0000

    Improved API, general syntax. Added clean up.

--- src/wymeditor/jquery.wymeditor.mozilla.js
+++ src/wymeditor/jquery.wymeditor.mozilla.js
@@ -18,30 +18,128 @@
 
 function WymClassMozilla(wym) {
 
-	this.wym = wym;
+	this._wym = wym;
 };
 
 WymClassMozilla.prototype.initIframe = function(iframe) {
 
-	this.iframe = iframe;
-	this.doc = iframe.contentDocument;
+	this._iframe = iframe;
+	this._doc = iframe.contentDocument;
 
-	this.doc.title = this.wym.index;
-	this.doc.designMode="on";
-	this.doc.execCommand("styleWithCSS",'',false);
+	this._doc.title = this._wym._index;
+	this._doc.designMode="on";
+	this._doc.execCommand("styleWithCSS",'',false);
 
-	$j(this.doc.body).html(this.wym.html);
+	this.html(this._wym._html);
 };
 
 WymClassMozilla.prototype._exec = function(cmd) {
 
-	this.doc.execCommand(cmd,'',null);
+	this._doc.execCommand(cmd,'',null);
 };
 
-WymClassMozilla.prototype.getContainer = function() {
+/* @name selected
+ * @description Returns the selected container
+ */
+WymClassMozilla.prototype.selected = function() {
 
-	var sel=this.iframe.contentWindow.getSelection();
+	var sel=this._iframe.contentWindow.getSelection();
 	var node=sel.focusNode;
 	if(node.nodeName=="#text")return(node.parentNode);
 	else return(node);
+};
+
+
+/* @name xhtml
+ * @description Cleans up the HTML
+ */
+WymClassMozilla.prototype.xhtml = function() {
+
+	var sHtml = this._wym.html();
+	
+	var flagTag=false,begTag=false;
+	var tag="",ret="",attr="";
+	var level=0;
+	
+	for(var x=0;x<sHtml.length;x++)
+	{
+		c=sHtml.charAt(x);
+		
+		if(begTag)
+		{
+			if(c==" " || c==">")
+			{
+				switch(tag)
+				{
+					case "b":
+						tag="strong";
+						break;
+					case "/b":
+						tag="/strong";
+						break;
+					case "i":
+						tag="em";
+						break;
+					case "/i":
+						tag="/em";
+						break;
+					case "br":
+						tag="br /"
+						break;
+					case "ol": case "ul":
+						level++;
+						if(level>1 && ret.substr(ret.length-5)=="</li>")
+							ret=ret.substr(0,ret.length-5);
+						break;
+					default:
+						break;
+				}
+				begTag=false;
+			}
+			else tag+=c;
+		}
+		
+		if(c=="<"){begTag=true;flagTag=true;}
+		
+		if(flagTag)
+		{
+			if(c==">")
+			{
+				flagTag=false;
+				switch(tag)
+				{
+					case "img":
+					case "hr":
+					case "input":
+					case "link":
+					case "base":
+					case "basefont":
+					case "col":
+					case "frame":
+					case "isindex":
+					case "meta":
+					case "param":
+						ret+="<"+tag+attr+"/>";
+						break;
+					case "/ol": case "/ul":
+						if(level>1)ret+="<"+tag+attr+"></li>";
+						else ret+="<"+tag+attr+">";
+						level--;
+						break;
+					default:
+						ret+="<"+tag+attr+">";
+						break;
+				}
+				
+				tag="";
+				attr="";
+			}
+			else if(!begTag)
+			{
+				attr+=c;
+			}
+		}
+		else ret+=c;	
+	}
+	return(ret);	
 };
