a4632e98a3d097bfab75d489f4aaeb5ecf1f56c0
Daniel Reszka <d.reszka@wymeditor.org>
2007-03-06 14:59:50 +0000

    skins/default/screen.css: added base styles for areas, sections, ... skins/default/main.js: added this file, which will allow for more advanced and crazy customization possibilities

---
+++ wymeditor/skins/default/main.js
@@ -0,0 +1,25 @@
+﻿/*
+ * WYMeditor : what you see is What You Mean web-based editor
+ * Copyright (C) 2007 H.O.net - http://www.honet.be/
+ * Dual licensed under the MIT (MIT-license.txt)
+ * and GPL (GPL-license.txt) licenses.
+ *
+ * For further information visit:
+ * 		http://www.wymeditor.org/
+ *
+ * File Name:
+ *		default.css
+ *		main javascript skin component for the default WYMeditor skin
+ *		See the documentation for more info.
+ *
+ * File Authors:
+ * 		Daniel Reszka (d.reszka@wymeditor.org)
+*/
+
+$j(function() {
+
+    $j("div.wym_containers").addClass("wym_panel");
+    $j("div.wym_tools").addClass("wym_buttons");
+	
+});
+    
