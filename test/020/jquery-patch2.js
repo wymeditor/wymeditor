8a8653267bcc93b90f28c27659b064c0d0e0b49a
Jean-Francois Hovinne <jf.hovinne@wymeditor.org>
2007-05-29 11:34:53 +0000

    test 020: added jQuery patch 2 (see #JQ1143).

---
+++ test/020/jquery-patch2.js
@@ -0,0 +1,20 @@
+﻿jQuery.merge = function(first, second) {
+    // We have to loop this way because IE & Opera overwrite the length
+    // expando of getElementsByTagName
+    for ( var i = 0; second[i]; i++ ) {
+    	second[i].$merge = true;
+    	first.push(second[i]);
+    }
+    return first;
+};
+
+jQuery.unique = function(first) {
+    var r = [];
+    for ( var i = 0, fl = first.length; i < fl; i++ )
+        if ( first[i].$merge ) {
+ 	      first[i].$merge = false;
+ 	      first[i].removeAttribute('$merge');
+ 	      r.push(first[i]);
+ 	    }
+ 	return r;
+};
