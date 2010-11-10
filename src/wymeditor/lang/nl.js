71bfa767581dcfa65b97821b4ca1818beac35360
marijn <marijn.huizendveld@gmail.com>
2010-11-10 15:46:11 +0100

    Updated Dutch translation file with the translation for the `Relationship` key.

--- src/wymeditor/lang/nl.js
+++ src/wymeditor/lang/nl.js
@@ -26,6 +26,7 @@ WYMeditor.STRINGS['nl'] = {
     Table_Header:     'Tabel-kop',
     URL:              'URL',
     Title:            'Titel',
+    Relationship:     'Relatie',
     Alternative_Text: 'Alternatieve tekst',
     Caption:          'Bijschrift',
     Summary:          'Summary',
@@ -42,4 +43,3 @@ WYMeditor.STRINGS['nl'] = {
     Status:           'Status',
     Source_Code:      'Broncode'
 };
-
