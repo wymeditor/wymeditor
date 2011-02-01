50ec4a3e96ad8ac6150737719f98fc4fd5c24c29
Samuel Cole <sam@samuelcole.name>
2011-02-01 15:07:01 -0500

    Stupid mistake, while fixing lint.

--- src/wymeditor/jquery.wymeditor.js
+++ src/wymeditor/jquery.wymeditor.js
@@ -389,7 +389,7 @@ jQuery.fn.wymeditor = function(options) {
                      "'><a href='#' name='" +
                      WYMeditor.TOOL_NAME +
                      "' title='" +
-                     WYMeditor.TOO +L_TITLE +
+                     WYMeditor.TOOL_TITLE +
                      "'>" +
                      WYMeditor.TOOL_TITLE +
                      "</a></li>",
