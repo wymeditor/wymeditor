9bb9bc96dc47f45db224b0644a77bd0211c2f5d9
Gellért Gyuris <bubu@ujevangelizacio.hu>
2012-02-14 13:53:47 +0100

    internationalisation of themes, pretty: 1st working concept

--- src/wymeditor/editor/firefox.js
+++ src/wymeditor/editor/firefox.js
@@ -53,6 +53,10 @@ WYMeditor.WymClassMozilla.prototype.initIframe = function (iframe) {
     //set the text direction
     jQuery('html', this._doc).attr('dir', this._options.direction);
 
+    //set lang; please add to other browser too
+    jQuery('html', this._doc).attr('lang', this._options.lang);
+    jQuery('html', this._doc).attr('xml:lang', this._options.lang);
+
     //init html value
     this.html(this._wym._html);
 
