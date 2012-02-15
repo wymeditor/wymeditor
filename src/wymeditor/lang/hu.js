be924e9b0bf553eb67adde4e86d6cc79ba4f5b42
Gellért Gyuris <bubu@ujevangelizacio.hu>
2012-02-16 00:16:09 +0100

    Use core language file for translation

--- src/wymeditor/lang/hu.js
+++ src/wymeditor/lang/hu.js
@@ -1,18 +1,18 @@
 WYMeditor.STRINGS.hu = {
-    Strong:           'Félkövér',
-    Emphasis:         'Kiemelt',
+    Strong:           'Félkövér', /* -> official name: strong emphasis */
+    Emphasis:         'Kiemelt', 
     Superscript:      'Felső index',
     Subscript:        'Alsó index',
-    Ordered_List:     'Rendezett lista',
-    Unordered_List:   'Rendezetlen lista',
-    Indent:           'Bekezdés',
-    Outdent:          'Bekezdés törlése',
-    Undo:             'Visszavon',
-    Redo:             'Visszaállít',
+    Ordered_List:     'Számozás',
+    Unordered_List:   'Felsorolás',
+    Indent:           'Behúzás',
+    Outdent:          'Behúzás törlése',
+    Undo:             'Visszavonás',
+    Redo:             'Újra',
     Link:             'Link',
     Unlink:           'Link törlése',
     Image:            'Kép',
-    Table:            'Tábla',
+    Table:            'Táblázat',
     HTML:             'HTML',
     Paragraph:        'Bekezdés',
     Heading_1:        'Címsor 1',
@@ -21,25 +21,63 @@ WYMeditor.STRINGS.hu = {
     Heading_4:        'Címsor 4',
     Heading_5:        'Címsor 5',
     Heading_6:        'Címsor 6',
-    Preformatted:     'Előformázott',
-    Blockquote:       'Idézet',
-    Table_Header:     'Tábla Fejléc',
+    Preformatted:     'Előformázott', /* -> official name: preformatted text */
+    Blockquote:       'Idézet', /* -> official name: block quotation */
+    Table_Header:     'Táblázat fejléc',
     URL:              'Webcím',
     Title:            'Megnevezés',
     Alternative_Text: 'Alternatív szöveg',
     Caption:          'Fejléc',
-    Summary:          'Summary',
+    Summary:          'Összegzés',
     Number_Of_Rows:   'Sorok száma',
     Number_Of_Cols:   'Oszlopok száma',
-    Submit:           'Elküld',
+    Submit:           'Elküldés',
     Cancel:           'Mégsem',
-    Choose:           'Választ',
+    Choose:           'Választás',
     Preview:          'Előnézet',
-    Paste_From_Word:  'Másolás Word-ból',
+    Paste_From_Word:  'Másolás Word-ből',
     Tools:            'Eszközök',
     Containers:       'Tartalmak',
     Classes:          'Osztályok',
     Status:           'Állapot',
-    Source_Code:      'Forráskód'
+    Source_Code:      'Forráskód',
+    
+    // tags
+	' type ':                 ', típusa: ',
+	'"\\""':                  '"”"', // after
+	'\\""':                   '„"',  // before
+	'document division':      'szakasz',
+	'ordered list':           'számozás', /* duplicate */
+	'unordered list':         'felsorolás', /* duplicate */
+	'definition list':        'definíciólista',
+	'list item':              'listaelem',
+	'definition term':        'definíciós kifejezés',
+	'definition description': 'definícióleírás',
+	'paragraph':              'bekezdés', /* duplicate */
+	'heading 1':              'címsor 1', /* duplicate */
+	'heading 2':              'címsor 2', /* duplicate */
+	'heading 3':              'címsor 3', /* duplicate */
+	'heading 4':              'címsor 4', /* duplicate */
+	'heading 5':              'címsor 5', /* duplicate */
+	'heading 6':              'címsor 6', /* duplicate */
+	'block quotation':        'idézetblokk', /* official name problem */
+	'preformatted text':      'előformázott szöveg',
+	'address or contact information': 'cím vagy elérhetőség',
+	'text span':              'szövegrész',
+	'strong emphasis':        'erős hangsúlyozás', /* official name problem */
+	'emphasis':               'hangsúlyozozás', /* official name problem */
+	'hyperlink':              'hiperhivatkozás',
+	'abbreviation':           'rövidítés',
+	'"acronym':               '"betűszó', // do not match 'acronym' in selector!
+	'keyboard input':         'felhasználói bevitel',
+	'work title':             'cím',
+	'sample text':            'mintaszöveg',
+	'variable':               'változó',
+	'deleted text':           'törölt szöveg',
+	'inserted text':          'beszúrt szöveg',
+	'definition':             'definíció',
+	'code fragment':          'kódrészlet',
+	'superscript':            'felső index', /* duplicate */
+	'subscript':              'alsó index' /* duplicate */
 };
 
