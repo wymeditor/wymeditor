689a09d0ae2fa933518a8c4d221467fef3c842d2
winhamwr <winhamwr@gmail.com>
2012-07-11 12:51:50 -0400

    Refs #360. Added a selenium test demonstrating the bug on chrome.

--- README.md
+++ README.md
@@ -134,6 +134,28 @@ to test with jQuery 1.8.0 against a local server on port 8000:
 
 [http://localhost:8000/test/unit/?jquery=1.8.0](http://localhost:8000/test/unit/?jquery=1.8.0).
 
+#### Selenium Tests
+
+Because WYMeditor is strongly affected by the way various browsers handle
+certain native events (keystrokes, mouse navigation, switching windows), it's
+not always possible to use javascript to actually test that behavior. For
+specific cases where it's not possible to reproduce behavior in javascript, we
+rely on our [http://seleniumhq.org/](Selenium) test suite to drive an actual
+browser using the [http://pypi.python.org/pypi/selenium](Selenium 2 python
+bindings).
+
+If possible, it is strongly encouraged to write a javascript unit test instead
+of a Selenium test.
+
+##### Installing Selenium Components
+
+1. Install the Selenium 2 python bindings, roughly following these
+[http://selenium-python.readthedocs.org/en/latest/installation.html](installation
+instructions). The specific version of Selenium we require can be installed via:
+
+	$ pip install -r wym_selenium/requirements.txt
+
+2.
 
 ### Building WYMeditor
 
---
+++ wym_selenium/test_root_containers.py
@@ -0,0 +1,107 @@
+"""
+Tests exercising the behavior of the root containers.
+"""
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+BASE_TEST_URL = 'http://localhost:8000/test'
+SPECIAL_KEYS = {
+    'backspace': u'\uE003',
+    'tab': u'\uE004',
+    'enter': u'\n',
+    'shift': u'\uE008',
+    'left': u'\uE012',
+    'up': u'\uE013',
+    'right': u'\uE014',
+    'down': u'\uE015',
+    'delete': u'\uE017',
+}
+
+def _get_driver():
+    return webdriver.Chrome()
+
+def _switch_to_editor_content(d):
+    d.switch_to_frame(0)
+
+def _switch_to_editor_controls(d):
+    d.switch_to_default_content()
+
+def _get_editor_body(d):
+    return d.find_element_by_xpath('//body')
+
+def _get_xhtml(d):
+    _switch_to_editor_controls(d)
+    html_button = d.find_element_by_class_name('wym_tools_html')
+    html_button.find_element_by_tag_name('a').click()
+
+    html_area = d.find_element_by_class_name('wym_html_val')
+    return html_area.get_attribute('value')
+
+def test_p_after_p():
+    # Ensure that hitting "Enter" after a p successfully creates a following p
+    # tag
+    d = _get_driver()
+
+    # Open the basic example page
+    d.get('%s/basic.html' % BASE_TEST_URL)
+    assert d.title == 'WYMeditor'
+
+    # Move the selection to the end of the first paragraph
+    _switch_to_editor_content(d)
+    body = _get_editor_body(d)
+    body.click()  # Put the cursor focus in the body
+    chain = ActionChains(d)
+    chain.send_keys(Keys.RIGHT * 20)
+    chain.perform()
+
+    # Hit return to create a new paragraph
+    chain.send_keys(Keys.RETURN)
+    chain.send_keys('2')
+    chain.perform()
+
+    html = _get_xhtml(d)
+    assert html == '<p>This is some text with which to test.</p><p>2</p>'
+
+    d.close()
+
+def test_p_after_h1():
+    # Ensure that hitting "Enter" after a h1 successfully creates a following p
+    # tag
+    d = _get_driver()
+
+    # Open the basic example page
+    d.get('%s/basic.html' % BASE_TEST_URL)
+    assert d.title == 'WYMeditor'
+
+    # Move the selection to the end of the first paragraph
+    _switch_to_editor_content(d)
+    body = _get_editor_body(d)
+    body.click()  # Put the cursor focus in the body
+    chain = ActionChains(d)
+    chain.send_keys(Keys.RIGHT * 20)
+    chain.perform()
+
+    # Turn the paragraph in to an h1
+    _switch_to_editor_controls(d)
+    chain = ActionChains(d)
+    containers_dropdown = d.find_element_by_class_name('wym_containers')
+    chain.move_to_element(containers_dropdown)
+    chain.perform()
+    containers_dropdown = d.find_element_by_class_name('wym_containers')
+    h1_container = containers_dropdown.find_element_by_class_name('wym_containers_h1')
+    chain.click(h1_container)
+    chain.perform()
+
+    # Hit return to create a new paragraph
+    _switch_to_editor_content(d)
+    chain = ActionChains(d)
+    chain.send_keys(Keys.RETURN)
+    chain.send_keys('2')
+    chain.perform()
+
+    html = _get_xhtml(d)
+    assert html == '<h1>This is some text with which to test.</h1><p>2</p>'
+
+    d.close()
+
