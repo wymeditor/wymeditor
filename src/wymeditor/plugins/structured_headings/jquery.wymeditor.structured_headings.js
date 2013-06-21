e541af4e49ddfdb4baa1593a66784f0aa700149f
FriedRice <n.mclaughlin22@yahoo.com>
2013-06-21 11:02:54 -0400

    Refs #409. Added structured_headings plugin. Numbered headings work for IE8 and above, Chrome, and Firefox.

---
+++ src/wymeditor/plugins/structured_headings/jquery.wymeditor.structured_headings.js
@@ -0,0 +1,62 @@
+/**
+    structuredHeadings
+    ==================
+
+    Initializes the structured_headings plugin for the wymeditor instance. This
+    function should be called in the `postInit` function of the wymeditor
+    instantiation.
+*/
+WYMeditor.editor.prototype.structuredHeadings = function () {
+    var wym = this,
+        stylesheetHref = '/plugins/structured_headings/structured_headings.css',
+        wymBasePath = WYMeditor.computeBasePath(WYMeditor.computeWymPath()),
+        iframeHead,
+        cssLink,
+        cssRequest;
+
+    if (jQuery.browser.msie && jQuery.browser.version < "8.0") {
+        // TODO: Use Javascript to add the header numbering on versions of IE
+        // before 8 because they don't support CSS counters.
+        wym.setupHeadingNumbering();
+
+    } else {
+        iframeHead = jQuery(wym._doc).find('head')[0];
+
+        cssLink = document.createElement('link');
+        cssLink.rel = 'stylesheet';
+        cssLink.type = 'text/css';
+        cssLink.href = '../..' + stylesheetHref; // Adjust path for iframe
+
+        iframeHead.appendChild(cssLink);
+
+        // Get stylesheet CSS and store it in WYMeditor so that it can be accessed
+        // to put on other pages.
+        cssRequest = new XMLHttpRequest();
+        cssRequest.open('GET', wymBasePath + stylesheetHref, false);
+        cssRequest.send('');
+        WYMeditor.structuredHeadingsCSS = cssRequest.responseText;
+    }
+};
+
+/**
+    WYMeditor.getStructuredHeadingsCSS
+    ==================================
+
+    Function to output the plugin CSS to the console log so that it can be
+    copied over to other pages.
+*/
+WYMeditor.getStructuredHeadingsCSS = function () {
+    if (!(jQuery.browser.msie && jQuery.browser.version < "8.0")) {
+        console.log(WYMeditor.structuredHeadingsCSS);
+    }
+};
+
+/**
+    setupHeadingNumbering
+    =====================
+
+    TODO: Javascript shim to add heading numbering to IE versions 7 and lower.
+*/
+WYMeditor.WymClassExplorer.prototype.setupHeadingNumbering = function () {
+    return false;
+}
