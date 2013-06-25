5283af2ece4d4a2cd0db66a5c98dddecd9e68eae
FriedRice <n.mclaughlin22@yahoo.com>
2013-06-25 09:17:45 -0400

    Refs #410. Began work on strcutured headings polyfill for IE7.

--- src/wymeditor/plugins/structured_headings/jquery.wymeditor.structured_headings.js
+++ src/wymeditor/plugins/structured_headings/jquery.wymeditor.structured_headings.js
@@ -8,23 +8,25 @@
 */
 WYMeditor.editor.prototype.structuredHeadings = function () {
     var wym = this,
-        stylesheetHref = '/plugins/structured_headings/structured_headings.css',
         wymBasePath = WYMeditor.computeBasePath(WYMeditor.computeWymPath()),
-        iframeHead,
+        iframeHead = jQuery(wym._doc).find('head')[0],
+        stylesheetHref,
         cssLink,
         cssRequest;
 
+    cssLink = wym._doc.createElement('link');
+    cssLink.rel = 'stylesheet';
+    cssLink.type = 'text/css';
+
     if (jQuery.browser.msie && jQuery.browser.version < "8.0") {
-        // TODO: Use Javascript to add the header numbering on versions of IE
-        // before 8 because they don't support CSS counters.
+        stylesheetHref = '/plugins/structured_headings/structured_headings_ie7.css';
+        cssLink.href = '../..' + stylesheetHref; // Adjust path for iframe
+        iframeHead.appendChild(cssLink);
+
         wym.setupHeadingNumbering();
 
     } else {
-        iframeHead = jQuery(wym._doc).find('head')[0];
-
-        cssLink = document.createElement('link');
-        cssLink.rel = 'stylesheet';
-        cssLink.type = 'text/css';
+        stylesheetHref = '/plugins/structured_headings/structured_headings.css';
         cssLink.href = '../..' + stylesheetHref; // Adjust path for iframe
 
         iframeHead.appendChild(cssLink);
@@ -57,6 +59,81 @@ WYMeditor.printStructuredHeadingsCSS = function () {
 
     TODO: Javascript shim to add heading numbering to IE versions 7 and lower.
 */
-WYMeditor.WymClassExplorer.prototype.setupHeadingNumbering = function () {
-    return false;
+WYMeditor.editor.prototype.setupHeadingNumbering = function () {
+    var wym = this,
+        $body = jQuery(wym._doc.body),
+        $head = jQuery(wym._doc).find('head'),
+        headerList = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'],
+        headerSel = headerList.join(', '),
+        prevHeaderTotal = 0;
+
+    $body.keydown(function () {
+        var $allHeaders,
+            $header,
+            headerNum,
+            headerLabel,
+            headerID,
+
+            span,
+            $span,
+            headerCSS,
+            $headCSS,
+
+            counters,
+            i,
+            j;
+
+        $allHeaders = $body.find(headerSel);
+        if ($allHeaders.length !== prevHeaderTotal) {
+
+            counters = [0, 0, 0, 0, 0, 0];
+            $body.find('.structured-heading-numbering').remove();
+
+            for (i = 0; i < $allHeaders.length; ++i) {
+                $header = $allHeaders.eq(i);
+                headerNum = parseInt($header[0].nodeName.slice(-1), 10) - 1;
+
+                ++counters[headerNum];
+                headerID = '';
+                for (j = 0; j <= headerNum; ++j) {
+                    if (j === headerNum) {
+                        headerID += counters[j];
+                    } else {
+                        headerID += counters[j] + '.';
+                    }
+                }
+                span = wym._doc.createElement('span');
+                $span = jQuery(span);
+                $header.prepend(span);
+                $span.html(headerID);
+                $span.attr('class', 'structured-heading-numbering');
+                $span.attr('contentEditable', 'false');
+
+                /*headerLabel = headerID.replace(/-/g, '.');
+                headerCSS = String() +
+                    '#h' + headerID + '{' +
+                        'zoom: expression( this.runtimeStyle["zoom"] = "1",' +
+                        'this.innerHTML = "' + headerLabel + ' "+this.innerHTML);' +
+                    '}';
+                $headCSS = $head.find('style#structured-headings-css');
+                if ($headCSS.length) {
+                    $headCSS[0].styleSheet.cssText += '\n' + headerCSS;
+                } else {
+                    style = wym._doc.createElement('style');
+
+                    style.setAttribute('id', 'structured-headings-css');
+                    style.setAttribute('type', 'text/css');
+                    style.styleSheet.cssText = headerCSS;
+
+                    $head[0].appendChild(style);
+                }*/
+
+                for (j = headerNum + 1; j < counters.length; ++j) {
+                    counters[j] = 0;
+                }
+            }
+
+            prevHeaderTotal = $allHeaders.length;
+        }
+    });
 }
