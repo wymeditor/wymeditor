fefcf2934592683c79d7cf366b450b3c697e82bf
FriedRice <n.mclaughlin22@yahoo.com>
2013-07-11 11:45:28 -0400

    Tweaked some grunt things. Need to upgrade qunit version next.

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,12 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         qunit: {
-            options: {
-                timeout: 20000
-            },
-            all: ['src/test/unit/index.html']
-        },
+            all: {
+                options: {
+                    urls: ['http://localhost:8000/test/unit/index.html?tapOutput=true']
+                }
+            }
+        }
     });
     grunt.loadNpmTasks('grunt-contrib-qunit');
     grunt.registerTask('default', ['qunit']);
