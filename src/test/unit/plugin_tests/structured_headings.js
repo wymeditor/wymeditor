46adb4296b2fd357a364e43edcb9b31360758302
FriedRice <n.mclaughlin22@yahoo.com>
2013-07-15 17:38:18 -0400

    Refs #417. Added tests for initializing the user interface and insertion of headings.

--- src/test/unit/plugin_tests/structured_headings.js
+++ src/test/unit/plugin_tests/structured_headings.js
@@ -28,7 +28,41 @@ function getHtmlAfterKeyup(wymeditor) {
         return bodyHtml;
 }
 
-module("structured_headings-initialize", {setup: setupWym});
+module("structured_headings-initialize_interface", {setup: setupWym});
+
+test("Single 'Heading' option in containers panel", function () {
+    expect(2);
+    var wymeditor = jQuery.wymeditors(0),
+        $box = jQuery(wymeditor._box),
+        $containersPanelItems = $box.find(
+            wymeditor._options.containersSelector + ' li a'),
+        containerName,
+        specificHeadingFound = false,
+        genericHeadingFound = false,
+        i;
+
+    for (i = 0; i < $containersPanelItems.length; ++i) {
+        containerName = $containersPanelItems[i].name.toLowerCase();
+        if (jQuery.inArray(containerName, WYMeditor.HEADING_ELEMENTS) > -1) {
+            specificHeadingFound = true;
+            break;
+        }
+    }
+    ok(!specificHeadingFound,
+       "No specific heading type option listed in the containers panel");
+
+    for (i = 0; i < $containersPanelItems.length; ++i) {
+        containerName = $containersPanelItems[i].name.toLowerCase();
+        if (containerName === "heading") {
+            genericHeadingFound = true;
+            break;
+        }
+    }
+    ok(genericHeadingFound,
+       "Generic heading option listed in the containers panel");
+});
+
+module("structured_headings-initialize_styles", {setup: setupWym});
 
 test("Stylesheet added to iframe", function () {
     expect(2);
@@ -90,6 +124,103 @@ test("CSS stored for user access through console", function () {
 
     deepEqual(WYMeditor.structuredHeadingsCSS, cssRequest.responseText,
               "CSS correctly stored for user access");
+});
+
+module("structured_headings-heading_insertion", {setup: setupWym});
+
+function testHeadingInsertion(wymeditor) {
+    expect(6);
+    var $body = jQuery(wymeditor._doc).find('body.wym_iframe'),
+        $headingContainerLink = jQuery(wymeditor._box).find(
+            wymeditor.structuredHeadingsManager._options.headingContainerPanelSelector),
+        paragraph,
+        i,
+        correctHtml = [
+            correctHtmlH1Insertion,
+            correctHtmlH2Insertion,
+            correctHtmlH3Insertion,
+            correctHtmlH4Insertion,
+            correctHtmlH5Insertion,
+            correctHtmlH6Insertion
+        ];
+
+    for (i = 1; i < 7; ++i) {
+        wymeditor._html(htmlForHeadingInsertion);
+        paragraph = $body.find('#to_be_h' + i)[0];
+        makeTextSelection(wymeditor, paragraph, paragraph);
+        $headingContainerLink.click();
+        htmlEquals(wymeditor, correctHtml[i - 1],
+                   "Insertion of an H" + i + " heading based on context.");
+    }
+}
+
+var htmlForHeadingInsertion = String() +
+    '<h1>H1</h1>' +
+    '<p id="to_be_h1">Test</p>' +
+        '<h2>H1_1</h2>' +
+            '<h3>H1_1_1</h3>' +
+            '<h3>H1_1_2</h3>' +
+        '<h2>H1_2</h2>' +
+        '<p id="to_be_h2">Test</p>' +
+            '<h3>H1_2_1</h3>' +
+            '<h3>H1_2_2</h3>' +
+                '<h4>H1_2_2_1</h4>' +
+                '<p id="to_be_h4">Test</p>' +
+                '<h4>H1_2_2_2</h4>' +
+    '<h1>H2</h1>' +
+        '<h2>H2_1</h2>' +
+            '<h3>H2_1_1</h3>' +
+                '<h4>H2_1_1_1</h4>' +
+                '<h4>H2_1_1_2</h4>' +
+                    '<h5>H2_1_1_2_1</h5>' +
+                    '<h5>H2_1_1_2_2</h5>' +
+                    '<p id="to_be_h5">Test</p>' +
+            '<h3>H2_1_2</h3>' +
+            '<p id="to_be_h3">Test</p>' +
+        '<h2>H2_2</h2>' +
+            '<h3>H2_2_1</h3>' +
+                '<h4>H2_2_1_1</h4>' +
+                    '<h5>H2_2_1_1_1</h5>' +
+                        '<h6>H2_2_1_1_1_1</h6>' +
+                        '<p id="to_be_h6">Test</p>' +
+                        '<h6>H2_2_1_1_1_2</h6>' +
+                    '<h5>H2_2_1_1_2</h5>' +
+                '<h4>H2_2_1_2</h4>' +
+            '<h3>H2_2_2</h3>';
+
+var correctHtmlH1Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h1">Test</p>', '<h1 id="to_be_h1">Test</h1>');
+
+var correctHtmlH2Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h2">Test</p>', '<h2 id="to_be_h2">Test</h2>');
+
+var correctHtmlH3Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h3">Test</p>', '<h3 id="to_be_h3">Test</h3>');
+
+var correctHtmlH4Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h4">Test</p>', '<h4 id="to_be_h4">Test</h4>');
+
+var correctHtmlH5Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h5">Test</p>', '<h5 id="to_be_h5">Test</h5>');
+
+var correctHtmlH6Insertion = htmlForHeadingInsertion.replace(
+    '<p id="to_be_h6">Test</p>', '<h6 id="to_be_h6">Test</h6>');
+
+test("Insert headings with defualt settings", function () {
+    expect(6);
+    var wymeditor = jQuery.wymeditors(0);
+
+    testHeadingInsertion(wymeditor);
+});
+
+test("Insert headings with custom highest and lowest heading levels", function () {
+    expect(6);
+    var wymeditor = jQuery.wymeditors(0);
+
+    wymeditor.structuredHeadingsManager._options.highestAllowableHeadingLevel = 2;
+    wymeditor.structuredHeadingsManager._options.lowestAllowableHeadingLevel = 4;
+
+    testHeadingInsertion(wymeditor);
 });
 
 // Tests for the IE7 polyfill
