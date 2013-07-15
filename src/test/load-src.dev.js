849e4afa9cd8fac675f09dd9347f5f43dc832dff
FriedRice <n.mclaughlin22@yahoo.com>
2013-07-15 15:41:05 -0400

    Refs #336. Changed the jQuery version in the project to 1.4.2.

--- src/test/load-src.dev.js
+++ src/test/load-src.dev.js
@@ -22,7 +22,7 @@ function parseVersionString(str) {
     head.js library. Loads relative to the given path to the src/ directory.
 */
 function loadWymSrc(srcPath, extraRequirements, jqueryVersion) {
-    var localJqueryVersion = "1.5.1",
+    var localJqueryVersion = "1.4.2",
         baseRequirements = [
             srcPath + 'wymeditor/rangy/rangy-core.js',
             srcPath + 'wymeditor/rangy/rangy-selectionsaverestore.js',
