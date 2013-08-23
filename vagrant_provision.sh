3bcc3d31022356435dae9c7bdd84dacafc0891ed
winhamwr <winhamwr@gmail.com>
2013-08-22 20:02:58 -0400

    Support WYMeditor development via Vagrant.

---
+++ vagrant_provision.sh
@@ -0,0 +1,16 @@
+# There are some packages that phantomjs requires and I'm currently too lazy to
+# script this via Chef
+sudo apt-get install libfontconfig1 fontconfig libfontconfig1-dev libfreetype6-dev
+
+# Set some environment variables so that the npm install works
+export NODE_PATH=/usr/lib/nodejs:/usr/lib/node_modules:/usr/share/javascript;
+export HOME=/home/vagrant;
+
+# Clean up any failed phantomjs installs
+rm -rf /tmp/phantomjs;
+
+# Let's install the NPM requirements
+sudo npm install -g grunt-cli;
+cd /home/vagrant/wym;
+npm install;
+
