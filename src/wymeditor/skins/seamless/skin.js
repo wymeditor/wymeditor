cc1c0a506f6f26d93c69faade4ae4101fb345b2e
winhamwr <winhamwr@gmail.com>
2013-09-11 14:55:43 -0400

    Center things, fix widths and remove borders for a more seamless experience.

--- src/wymeditor/skins/seamless/skin.js
+++ src/wymeditor/skins/seamless/skin.js
@@ -1,41 +1,25 @@
 WYMeditor.SKINS.seamless = {
     init: function (wym) {
-        //render following sections as panels
-        jQuery(wym._box).find(wym._options.classesSelector)
-          .addClass("wym_panel");
-
-        //render following sections as buttons
-        jQuery(wym._box).find(wym._options.toolsSelector)
-          .addClass("wym_buttons");
-
-        //render following sections as dropdown menus
-        jQuery(wym._box).find(wym._options.containersSelector)
-          .addClass("wym_dropdown")
-          .find(WYMeditor.H2)
-          .append("<span> ></span>");
-
-        // auto add some margin to the main area sides if left area
-        // or right area are not empty (if they contain sections)
-        jQuery(wym._box).find("div.wym_area_right ul")
-          .parents("div.wym_area_right").show()
-          .parents(wym._options.boxSelector)
-          .find("div.wym_area_main")
-          .css({"margin-right": "155px"});
-
-        jQuery(wym._box).find("div.wym_area_left ul")
-          .parents("div.wym_area_left").show()
-          .parents(wym._options.boxSelector)
-          .find("div.wym_area_main")
-          .css({"margin-left": "155px"});
-
-        //make hover work under IE < 7
-        jQuery(wym._box).find(".wym_section").hover(
-            function () {
-                jQuery(this).addClass("hover");
-            },
-            function () {
-                jQuery(this).removeClass("hover");
+        // The classes and containers sections are dropdowns to the right of
+        // the toolbar at the top
+        jQuery(
+            wym._options.containersSelector + ', ' +
+            wym._options.classesSelector,
+            wym._box
+        ).appendTo(
+            jQuery("div.wym_area_top", wym._box)
+        ).addClass("wym_dropdown")
+          .css(
+            {
+                "margin-right": "10px",
+                "width": "120px",
+                "float": "left"
             }
-        );
+          );
+
+        // The toolbar uses buttons
+        jQuery(wym._options.toolsSelector, wym._box)
+          .addClass("wym_buttons")
+          .css({"margin-right": "10px", "float": "left"});
     }
 };
