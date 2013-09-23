4dd2a36150a5bb8b98b499b8f1c5e26299b1274c
winhamwr <winhamwr@gmail.com>
2013-09-23 17:38:47 -0400

    Use setTimeout to work around IE7's iframe initialization problem.

--- src/wymeditor/skins/seamless/skin.js
+++ src/wymeditor/skins/seamless/skin.js
@@ -1,8 +1,27 @@
 WYMeditor.SKINS.seamless = {
+    OPTS: {
+        iframeHtml: String() +
+            '<div class="wym_iframe wym_section">' +
+                '<iframe src="' + WYMeditor.IFRAME_BASE_PATH + 'wymiframe.html" ' +
+                    'frameborder="0" ' +
+                    'scrolling="no" ' +
+                    'onload="this.contentWindow.parent.WYMeditor.INSTANCES[' +
+                    WYMeditor.INDEX + '].initIframe(this)"' +
+                    '>' +
+                '</iframe>' +
+            "</div>",
+        // After Iframe initialization, check if we're ready to perform the
+        // first resize every this many ms
+        initIframeCheckFrequency: 50
+    },
     init: function (wym) {
         var This = WYMeditor.SKINS.seamless;
 
-        This.initChrome(wym);
+        wym.seamlessSkinOpts = {
+            initIframeCheckFrequency: This.initIframeCheckFrequency,
+            initialIframeResizeTimer: null
+        };
+        This.initUIChrome(wym);
 
         // The Iframe isn't initialized at this point, so we'll need to wait
         // until it is before attempting to use it.
@@ -15,9 +34,7 @@ WYMeditor.SKINS.seamless = {
         var This = WYMeditor.SKINS.seamless;
 
         // Perform an initial resize, if necessary
-        jQuery(wym._doc).ready(function () {
-            This.resizeIframe(wym);
-        });
+        This.resizeIframeOnceBodyExists(wym);
 
         // Detect possible Block creation so that we can always keep the iframe
         // properly resized and the current container in view
@@ -26,7 +43,7 @@ WYMeditor.SKINS.seamless = {
             This.resizeAndScrollIfNeeded
         );
     },
-    initChrome: function (wym) {
+    initUIChrome: function (wym) {
         // Initialize the toolbar and classes/containers selectors
 
         // The classes and containers sections are dropdowns to the right of
@@ -50,6 +67,41 @@ WYMeditor.SKINS.seamless = {
         jQuery(wym._options.toolsSelector, wym._box)
           .addClass("wym_buttons")
           .css({"margin-right": "10px", "float": "left"});
+    },
+    resizeIframeOnceBodyExists: function (wym) {
+        // In IE, the wym._doc DOMLoaded event doesn't mean the iframe will
+        // actually have a body. We need to wait until the body actually exists
+        // before trying to set the initial hight of the iframe, so we hack
+        // this together with setTimeout.
+        var This = WYMeditor.SKINS.seamless,
+            scrollHeightCalcFix;
+
+        if (wym.seamlessSkinOpts.initialIframeResizeTimer) {
+            // We're handling a timer, clear it
+            window.clearTimeout(
+                wym.seamlessSkinOpts.initialIframeResizeTimer
+            );
+            wym.seamlessSkinOpts.initialIframeResizeTimer = null;
+        }
+
+        if (typeof wym._doc.body === "undefined" || wym._doc.body === null) {
+            // Body isn't ready
+            wym.seamlessSkinOpts.initialIframeResizeTimer = window.setTimeout(
+                function () {
+                    This.resizeIframeOnceBodyExists(wym);
+                },
+                wym.seamlessSkinOpts.initIframeCheckFrequency
+            );
+            return;
+        }
+        // The body is ready, so let's get to resizing
+        // For some reason, at least IE7 requires at least one access to the
+        // scrollHeight before it gives a real value. I was unable to find any
+        // kind of feature detection that would work here and the fix of adding
+        // an extra access here doesn't have any real negative impact on the
+        // other browsers, but does fix IE7's behavior.
+        scrollHeightCalcFix = wym._doc.body.scrollHeight;
+        This.resizeIframe(wym);
     },
     resizeIframe: function (wym) {
         var desiredHeight = wym._doc.body.scrollHeight,
@@ -104,17 +156,5 @@ WYMeditor.SKINS.seamless = {
             return;
         }
         This.scrollIfNeeded(wym);
-    },
-    WYM_OPTS: {
-        iframeHtml: String() +
-            '<div class="wym_iframe wym_section">' +
-                '<iframe src="' + WYMeditor.IFRAME_BASE_PATH + 'wymiframe.html" ' +
-                    'frameborder="0" ' +
-                    'scrolling="no" ' +
-                    'onload="this.contentWindow.parent.WYMeditor.INSTANCES[' +
-                    WYMeditor.INDEX + '].initIframe(this)"' +
-                    '>' +
-                '</iframe>' +
-            "</div>",
     }
 };
