54bd390a8a6749b8d3f83ee377f333990b680abf
winhamwr <winhamwr@gmail.com>
2013-09-25 12:49:22 -0400

    Cleaned up some of the odd jquery-specific spacing in the seamless skin.

--- src/wymeditor/skins/seamless/skin.js
+++ src/wymeditor/skins/seamless/skin.js
@@ -51,25 +51,29 @@ WYMeditor.SKINS.seamless = {
 
         // The classes and containers sections are dropdowns to the right of
         // the toolbar at the top
-        jQuery(
-            wym._options.containersSelector + ', ' +
-            wym._options.classesSelector,
+        var $dropdowns = jQuery(
+            [
+                wym._options.containersSelector
+                , wym._options.classesSelector
+            ].join(', '),
             wym._box
-        ).appendTo(
+        ),
+            $toolbar;
+
+        $dropdowns.appendTo(
             jQuery("div.wym_area_top", wym._box)
-        ).addClass("wym_dropdown")
-          .css(
-            {
-                "margin-right": "10px",
-                "width": "120px",
-                "float": "left"
-            }
-          );
+        );
+        $dropdowns.addClass("wym_dropdown");
+        $dropdowns.css({
+            "margin-right": "10px",
+            "width": "120px",
+            "float": "left"
+        });
 
         // The toolbar uses buttons
-        jQuery(wym._options.toolsSelector, wym._box)
-          .addClass("wym_buttons")
-          .css({"margin-right": "10px", "float": "left"});
+        $toolbar = jQuery(wym._options.toolsSelector, wym._box);
+        $toolbar.addClass("wym_buttons");
+        $toolbar.css({"margin-right": "10px", "float": "left"});
     },
     resizeIframeOnceBodyExists: function (wym) {
         // In IE, the wym._doc DOMLoaded event doesn't mean the iframe will
