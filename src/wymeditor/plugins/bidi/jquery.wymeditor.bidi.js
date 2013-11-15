8ce338ec7c56145763a761232f86a0e457f6fa3a
Shahar Or <mightyiampresence@gmail.com>
2013-11-15 10:51:23 +0200

    Partial rewrite to adhere to attempt to follow coding standards.

--- src/wymeditor/plugins/bidi/jquery.wymeditor.bidi.js
+++ src/wymeditor/plugins/bidi/jquery.wymeditor.bidi.js
@@ -6,63 +6,63 @@
     A plugin to control the `dir` attribute of elements.
 
     By Shahar Or (mightyiampresence a-t gmail dotcom)
-    Mostly hacked from https://bitbucket.org/Patabugen/wymeditor-plugins/src/01595ab6ef37/plugins/alignment?at=master
+    Mostly hacked from Patabugen's wymeditor-plugins alignment.
 */
 
 WYMeditor.editor.prototype.bidi = function () {
-    var wym = this,
-        $box = jQuery(this._box);
+    "use strict";
 
-    options = {
+    var wym, box, buttonLtr, buttonRtl, buttonRmDir, html;
 
-    }
+    wym = this;
+    box = jQuery(this._box);
 
     //construct the buttons' html
-    var button_ltr = String() +
-        "<li class='wym_tools_direction_ltr'>" +
-            "<a name='DirectionLtr' href='#' " +
-                "style='background-image: url(" +
-                    wym._options.basePath +
-                    "plugins/bidi/icons.png)'>" +
-                "{ltr}" +
-            "</a>" +
-        "</li>";
-    var button_rtl = String() +
-        "<li class='wym_tools_direction_rtl'>" +
-            "<a name='DirectionRtl' href='#' " +
-                "style='background-image: url(" +
-                    wym._options.basePath +
-                    "plugins/bidi/icons.png); background-position: 0px -24px'>" +
-                "{rtl}" +
-            "</a>" +
-        "</li>";
-    var button_rmdir = String() +
-        "<li class='wym_tools_direction_remove'>" +
-            "<a name='DirectionRemove' href='#' " +
-                "style='background-image: url(" +
-                    wym._options.basePath +
-                    "plugins/bidi/icons.png); background-position: 0px -48px'>" +
-                "{rmdir}" +
-            "</a>" +
-        "</li>";
+    buttonLtr = [""
+        , "<li class='wym_tools_direction_ltr'>"
+        , "<a name='ltr' href='#'"
+        , " style='background-image: url("
+        , wym._options.basePath
+        , "plugins/bidi/icons.png);'>"
+        , "{ltr}</a></li>"
+    ].join('');
+    buttonRtl = [""
+        , "<li class='wym_tools_direction_rtl'>"
+        , "<a name='rtl' href='#'"
+        , " style='background-image: url("
+        , wym._options.basePath
+        , "plugins/bidi/icons.png);"
+        , " background-position: 0px -24px;'>"
+        , "{rtl}</a></li>"
+    ].join('');
+    buttonRmDir = [""
+        , "<li class='wym_tools_direction_remove'>"
+        , "<a name='rmdir' href='#'"
+        , " style='background-image: url("
+        , wym._options.basePath
+        , "plugins/bidi/icons.png);"
+        , " background-position: 0px -48px;'>"
+        , "{rmdir}</a></li>"
+    ].join('');
 
-    var html = button_ltr + button_rtl + button_rmdir;
+    html = buttonLtr + buttonRtl + buttonRmDir;
+
     //add the button to the tools box
-    $box.find(wym._options.toolsSelector + wym._options.toolsListSelector)
+    box.find(wym._options.toolsSelector + wym._options.toolsListSelector)
         .append(html);
 
-    $box.find('li.wym_tools_direction_ltr a').click(function() {
+    box.find('li.wym_tools_direction_ltr a').click(function () {
         var container = wym.container();
         $(container).attr('dir', 'ltr');
 
         return false;
     });
-    $box.find('li.wym_tools_direction_rtl a').click(function() {
+    box.find('li.wym_tools_direction_rtl a').click(function () {
         var container = wym.container();
         $(container).attr('dir', 'rtl');
         return false;
     });
-    $box.find('li.wym_tools_direction_remove a').click(function() {
+    box.find('li.wym_tools_direction_remove a').click(function () {
         var container = wym.container();
         $(container).removeAttr('dir');
         return false;
