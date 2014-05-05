cb9ba84df1b30d41b87f94e2f0002374c455a835
Shahar Or <mightyiampresence@gmail.com>
2014-05-05 12:19:49 +0300

    Fix: Some unit tests' string of expected HTML was missing a closing `li`.

--- src/test/unit/specific_feature_tests/xml_parser.js
+++ src/test/unit/specific_feature_tests/xml_parser.js
@@ -1420,6 +1420,7 @@ var unwrapMultiInNestedListHtml = String() +
                     '</ol>' +
                 '</li>' +
             '</ul>' +
+        '</li>' +
         '<li><{blockTag}>Test</{blockTag}></li>' +
     '</ol>';
 
