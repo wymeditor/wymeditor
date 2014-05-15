17ca0ab1604c16a9f1b4ada30b17fd9af68ae8b0
Shahar Or <mightyiampresence@gmail.com>
2014-05-15 17:08:57 +0300

    NON_CARET_ELEMENTS constant: removed 'th' and added 'thead' and 'colgroup'.

--- src/wymeditor/core.js
+++ src/wymeditor/core.js
@@ -251,7 +251,7 @@ jQuery.extend(WYMeditor, {
 
     // Elements that should not contain collapsed selection directly.
     NO_CARET_ELEMENTS: ["br", "hr", "img", "ul", "ol", "table", "dl", "tbody",
-        "th", "tr"],
+        "thead", "tr", "colgroup"],
 
     // Inline elements.
     INLINE_ELEMENTS : ["b", "big", "i", "small", "tt", "abbr", "acronym",
