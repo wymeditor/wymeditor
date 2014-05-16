762e6a4de759837b2969c600450d5de97e4fcabd
Shahar Or <mightyiampresence@gmail.com>
2014-05-16 15:29:58 +0300

    NO_CARET_ELEMENTS: Removed 'body'. Added 'col' and 'tfoot'.

--- src/wymeditor/core.js
+++ src/wymeditor/core.js
@@ -251,7 +251,7 @@ jQuery.extend(WYMeditor, {
 
     // Elements that should not contain collapsed selection directly.
     NO_CARET_ELEMENTS: ["br", "hr", "img", "ul", "ol", "table", "dl", "tbody",
-        "thead", "tr", "colgroup", "blockquote", "body"],
+        "thead", "tr", "colgroup", "blockquote", "col", "tfoot"],
 
     // Inline elements.
     INLINE_ELEMENTS : ["b", "big", "i", "small", "tt", "abbr", "acronym",
