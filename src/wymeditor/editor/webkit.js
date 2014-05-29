74f1e31d8a2bbc44292df5449726f49bbbb6e11f
Shahar Or <mightyiampresence@gmail.com>
2014-05-29 19:37:45 +0300

    In initial HTML loading, prefer textarea content over options.

--- src/wymeditor/editor/webkit.js
+++ src/wymeditor/editor/webkit.js
@@ -22,7 +22,11 @@ WYMeditor.WymClassWebKit.prototype.initIframe = function (iframe) {
     wym._doc.designMode = "on";
 
     // Init html value
-    wym._html(wym._wym._options.html);
+    if (wym._element[0].value) {
+        wym._html(wym._element[0].value);
+    } else {
+        wym._html(wym._wym._options.html);
+    }
 
     if (jQuery.isFunction(wym._options.preBind)) {
         wym._options.preBind(wym);
