22a04933f74d75e94262ac680dc7b3ca8f303d86
Shahar Or <mightyiampresence@gmail.com>
2014-06-01 16:07:59 +0300

    Issue #450: Empty is empty with caret inside the body as well.

--- src/test/unit/specific_feature_tests/xml_parser.js
+++ src/test/unit/specific_feature_tests/xml_parser.js
@@ -8,8 +8,14 @@ module("XmlParser", {setup: setupWym});
 
 test("Empty is empty", function () {
     var wymeditor = jQuery.wymeditors(0);
+    expect(2);
 
     wymeditor._html('');
+    wymEqual(wymeditor, '');
+
+    wymeditor._html('');
+    // Placing the caret inside shouldn't create any content.
+    wymeditor.setCaretIn(jQuery(wymeditor._doc).find('body.wym_iframe')[0]);
     wymEqual(wymeditor, '');
 });
 
