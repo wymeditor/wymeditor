078d754f4b82652c52b2a43f3b1c59167965b9a5
Shahar Or <mightyiampresence@gmail.com>
2014-07-03 16:20:03 +0300

    Fix table test "Tab from th to next row" for WebKit/Blink.

--- src/test/unit/plugin_tests/table_test.js
+++ src/test/unit/plugin_tests/table_test.js
@@ -859,7 +859,6 @@ test("Tab from th to next row", function () {
     expect(3);
     var expectedSelector = '#span_2_1';
     if (jQuery.browser.mozilla ||
-        (jQuery.browser.webkit && parseInt(jQuery.browser.version, 10) > 536) ||
         (jQuery.browser.msie && parseInt(jQuery.browser.version, 10) >= 9.0)
     ) {
         expectedSelector = '#td_2_1';
