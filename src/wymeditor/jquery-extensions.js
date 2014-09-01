9ed0dbefea824740c3c86612f822b212ef007725
Shahar Or <mightyiampresence@gmail.com>
2014-09-01 14:25:24 +0300

    Add jQuery.replaceAllInStr.

--- src/wymeditor/jquery-extensions.js
+++ src/wymeditor/jquery-extensions.js
@@ -6,4 +6,11 @@
  */
 
 jQuery.extend({
+    /**
+     * Replace all instances of 'old' by 'rep' in 'str' string.
+     */
+    replaceAllInStr: function (str, old, rep) {
+        var rExp = new RegExp(old, "g");
+        return str.replace(rExp, rep);
+    }
 });
