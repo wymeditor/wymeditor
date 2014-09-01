441dfe6dc3b7b0906bd827b0811b969d2cf403ca
Shahar Or <mightyiampresence@gmail.com>
2014-09-01 13:45:24 +0300

    Remove jQuery.arrayIndexOf.

--- src/wymeditor/jquery-extensions.js
+++ src/wymeditor/jquery-extensions.js
@@ -6,20 +6,4 @@
  */
 
 jQuery.extend({
-    /**
-     * Return 'item' position in 'arr' array, or -1.
-     * This is similar but probably not identical to ES5
-     * Array.prorotype.indexOf.
-     * For example, there seems to be no handling of negative indices.
-     */
-    arrayIndexOf: function (arr, item) {
-        var ret = -1, i;
-        for (i = 0; i < arr.length; i += 1) {
-            if (arr[i] === item) {
-                ret = i;
-                break;
-            }
-        }
-        return ret;
-    }
 });
