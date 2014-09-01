d48d39242dc80cd26f97e62d3c3bab5f2ac9f1a6
Shahar Or <mightyiampresence@gmail.com>
2014-09-01 19:44:45 +0300

    Added jQuery.arrayContains.

--- src/wymeditor/jquery-extensions.js
+++ src/wymeditor/jquery-extensions.js
@@ -6,4 +6,16 @@
  */
 
 jQuery.extend({
+    /*
+     * Return true if 'arr' array contains 'item', or false
+     */
+    arrayContains: function (arr, item) {
+        var i;
+        for (i = 0; i < arr.length; i += 1) {
+            if (arr[i] === item) {
+                return true;
+            }
+        }
+        return false;
+    }
 });
