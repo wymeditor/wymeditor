f25ac3a4fcad82e2acaf231d05bed5f472c87902
Shahar Or <mightyiampresence@gmail.com>
2014-09-01 20:00:04 +0300

    New jQuery.getFromArrayByName

--- src/wymeditor/jquery-extensions.js
+++ src/wymeditor/jquery-extensions.js
@@ -6,4 +6,18 @@
  */
 
 jQuery.extend({
+    /**
+     * Return 'item' object in 'arr' array, checking its 'name' property.
+     * No such item returns null.
+     */
+    getFromArrayByName: function (arr, name) {
+        var i, item;
+        for (i = 0; i < arr.length; i += 1) {
+            item = arr[i];
+            if (item.name === name) {
+                return item;
+            }
+        }
+        return null;
+    }
 });
