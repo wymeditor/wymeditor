a52296d0533ae7d99bff8ed3a2bfa6d69caef4cb
Shahar Or <mightyiampresence@gmail.com>
2014-10-25 17:11:16 +0300

    Add undo/redo tests for list actions and selection restoration.

---
+++ src/test/unit/specific_feature_tests/undo_redo.js
@@ -0,0 +1,117 @@
+/* jshint evil: true */
+/* global
+    test,
+    wymEqual,
+    prepareUnitTestModule,
+    makeTextSelection,
+    ok
+*/
+"use strict";
+
+module("undo_redo", {setup: prepareUnitTestModule});
+
+test("Can undo and redo all the things", function () {
+    // This tests:
+    // #. Insertion of ordered list.
+    // #. Insertion of unordered list.
+    // #. Indenting.
+    // #. Outdenting.
+    // #. That selection is restored.
+    var wymeditor = jQuery.wymeditors(0),
+        $body = wymeditor.$body(),
+        $buttons = wymeditor.get$Buttons(),
+        $undoButton = $buttons.filter('[name=Undo]'),
+        $redoButton = $buttons.filter('[name=Redo]'),
+        undo = function () {wymeditor.undoRedo.undo(); },
+        redo = function () {wymeditor.undoRedo.redo(); },
+        selection;
+
+    wymeditor.html("<p>Foo</p>");
+    wymeditor.undoRedo._add();
+    wymeditor.setCaretIn($body.children('p')[0]);
+    wymeditor.exec('InsertOrderedList');
+    wymEqual(
+        wymeditor,
+        "<ol><li>Foo</li></ol>"
+    );
+    wymeditor.exec('InsertUnorderedList');
+    wymEqual(
+        wymeditor,
+        "<ul><li>Foo</li></ul>"
+    );
+    // Use the button once.
+    $undoButton.click();
+    wymEqual(
+        wymeditor,
+        "<ol><li>Foo</li></ol>"
+    );
+    // Use the function from now on for performance.
+    undo();
+    wymEqual(
+        wymeditor,
+        "<p>Foo</p>"
+    );
+    undo();
+    wymEqual(
+        wymeditor,
+        ""
+    );
+    $redoButton.click();
+    wymEqual(
+        wymeditor,
+        "<p>Foo</p>"
+    );
+    redo();
+    wymEqual(
+        wymeditor,
+        "<ol><li>Foo</li></ol>"
+    );
+    redo();
+    wymEqual(
+        wymeditor,
+        "<ul><li>Foo</li></ul>"
+    );
+    makeTextSelection(
+        wymeditor,
+        $body.find("li")[0],
+        $body.find("li")[0],
+        0,
+        3
+    );
+    wymeditor.exec('Indent');
+    wymEqual(
+        wymeditor,
+        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
+    );
+    $body.children('ul').after('<p>Bar</p>');
+    wymeditor.setCaretIn($body.children('p')[0]);
+    wymeditor.undoRedo._add();
+    undo();
+    wymEqual(
+        wymeditor,
+        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
+    );
+    selection = wymeditor.selection();
+    equal(
+        selection.anchorNode,
+        $body.find('li li')[0]
+    );
+    equal(
+        selection.focusNode,
+        $body.find('li li')[0]
+    );
+    equal(
+        wymeditor.selection().anchorOffset,
+        0
+    );
+    equal(
+        wymeditor.selection().focusOffset,
+        1
+    );
+    wymeditor.exec('OutDent');
+    undo();
+    wymEqual(
+        wymeditor,
+        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
+    );
+});
