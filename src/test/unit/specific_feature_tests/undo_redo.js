9adf5c6ff1425a307c91172fa35d8d32266deefb
Shahar Or <mightyiampresence@gmail.com>
2014-10-31 11:36:44 +0200

    Rewrite Undo/Redo tests.

--- src/test/unit/specific_feature_tests/undo_redo.js
+++ src/test/unit/specific_feature_tests/undo_redo.js
@@ -9,115 +9,287 @@
 */
 "use strict";
 
+/**
+ * testUndoRedo
+ * ============
+ *
+ * Tests Undo/Redo functionality.
+ *
+ * @param a An object, containing:
+ *     `testName`
+ *         A name for the test.
+ *     `startHtml`
+ *         HTML to start the test with. Required if `expectedStartHtml` is not
+ *         used;
+ *     `setCaretInSelector`
+ *         Optional; jQuery selector for an element to set the caret in at the
+ *         start of the test.
+ *     `prepareFunc`
+ *         Optional; A function to prepare the test. Receives one argument, the
+ *         WYMeditor instance.
+ *     `expectedStartHtml`
+ *         The HTML that is expected to be the state of the document after the
+ *         `prepareFunc` ran. If this is not provided, the value of `startHtml`
+ *         will be used.
+ *     `manipulationFunc`
+ *         The manipulation function to be tested. Receives one argument, the
+ *         WYMeditor instance.
+ *     `expectedResultHtml`
+ *         The HTML that is expected to be the state of the document after the
+ *         `manipulationFunc` ran.
+ *     `additionalAssertionsFunc`
+ *         Optional; Additional assertions for after the `manipulationFunc`.
+ */
+function testUndoRedo(a) {
+    test(a.testName, function () {
+        var wymeditor = jQuery.wymeditors(0);
+        if (a.startHtml) {
+            wymeditor.html(a.startHtml);
+        }
+        if (a.setCaretInSelector) {
+            wymeditor.setCaretIn(
+                wymeditor.$body().find(a.setCaretInSelector)[0]
+            );
+        }
+        if (a.prepareFunc) {
+            a.prepareFunc(wymeditor);
+        }
+        wymeditor.undoRedo.reset();
+        wymEqual(wymeditor, a.expectedStartHtml || a.startHtml);
+
+        a.manipulationFunc(wymeditor);
+        wymEqual(wymeditor, a.expectedResultHtml);
+        if (a.additionalAssertionsFunc) {
+            a.additionalAssertionsFunc(wymeditor);
+        }
+
+        wymeditor.undoRedo.undo();
+        wymEqual(wymeditor, a.expectedStartHtml || a.startHtml);
+
+        wymeditor.undoRedo.redo();
+        wymEqual(wymeditor, a.expectedResultHtml);
+        if (a.additionalAssertionsFunc) {
+            a.additionalAssertionsFunc(wymeditor);
+        }
+    });
+}
+
 module("undo_redo", {setup: prepareUnitTestModule});
 
-test("Can undo and redo all the things", function () {
-    // This tests:
-    //
-    // #. Insertion of ordered list.
-    // #. Insertion of unordered list.
-    // #. Indenting.
-    // #. Outdenting.
-    // #. Native `execCommand` commands.
-    // #. That selection is restored.
-    //
-    // This does not test dialog window actions:
-    //
-    // #. Link
-    // #. Image
-    // #. Table
-    // #. Paste
-    expect(13);
-    var wymeditor = jQuery.wymeditors(0),
-        $body = wymeditor.$body(),
-        $buttons = wymeditor.get$Buttons(),
-        $undoButton = $buttons.filter('[name=Undo]'),
-        $redoButton = $buttons.filter('[name=Redo]'),
-        undo = function () {wymeditor.undoRedo.undo(); },
-        redo = function () {wymeditor.undoRedo.redo(); },
-        selection;
+testUndoRedo({
+    testName: "List; InsertOrderedList",
+    startHtml: "<p>Foo</p>",
+    setCaretInSelector: "p",
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("InsertOrderedList");
+    },
+    expectedResultHtml: "<ol><li>Foo</li></ol>"
+});
+
+testUndoRedo({
+    testName: "List; InsertUnorderedList",
+    startHtml: "<p>Foo</p>",
+    setCaretInSelector: "p",
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("InsertUnorderedList");
+    },
+    expectedResultHtml: "<ul><li>Foo</li></ul>"
+});
+
+testUndoRedo({
+    testName: "List; Indent",
+    startHtml: "<ol><li>Foo</li></ol>",
+    setCaretInSelector: "li",
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Indent");
+    },
+    expectedResultHtml:
+        "<ol><li class=\"spacer_li\"><ol><li>Foo</li></ol></li></ol>"
+});
+
+testUndoRedo({
+    testName: "List; Outdent",
+    startHtml: "<ol><li class=\"spacer_li\"><ol><li>Foo</li></ol></li></ol>",
+    setCaretInSelector: "li li",
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Outdent");
+    },
+    expectedResultHtml:
+        "<ol><li>Foo</li></ol>"
+});
+
+var DAWN_OF_HISTORY = "<h1>Dawn of History</h1>";
+
+testUndoRedo({
+    testName: "No going back before dawn of history",
+    startHtml: DAWN_OF_HISTORY,
+    manipulationFunc: function () {},
+    additionalAssertionsFunc: function (wymeditor) {
+        wymEqual(wymeditor, DAWN_OF_HISTORY);
+    },
+    expectedResultHtml: DAWN_OF_HISTORY
+});
+
+testUndoRedo({
+    testName: "Restores selection",
+    startHtml: "<p>Foo</p><p>Bar</p>",
+    prepareFunc: function (wymeditor) {
+        makeTextSelection(
+            wymeditor,
+            wymeditor.$body().children('p')[0],
+            wymeditor.$body().children('p')[1],
+            0,
+            3
+        );
+    },
+    manipulationFunc: function () {},
+    additionalAssertionsFunc: function (wymeditor) {
+        equal(
+            wymeditor.selection().toString(),
+            "FooBar"
+        );
+    },
+    expectedResultHtml: "<p>Foo</p><p>Bar</p>"
+});
+
+test("Redo when everything has been redone", function () {
+    expect(5);
+
+    var wymeditor = jQuery.wymeditors(0);
 
     wymeditor.html("<p>Foo</p>");
     wymeditor.undoRedo.reset();
-    wymeditor.setCaretIn($body.children('p')[0]);
-    wymeditor.exec('InsertOrderedList');
-    wymEqual(
-        wymeditor,
-        "<ol><li>Foo</li></ol>"
-    );
-    wymeditor.exec('InsertUnorderedList');
-    wymEqual(
-        wymeditor,
-        "<ul><li>Foo</li></ul>"
-    );
-    // Use the button once.
+    wymEqual(wymeditor, "<p>Foo</p>");
+
+    wymeditor.$body().append("<p>Bar</p>");
+    wymeditor.registerChange();
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+
+    wymeditor.undoRedo.undo();
+    wymEqual(wymeditor, "<p>Foo</p>");
+
+    wymeditor.undoRedo.redo();
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+
+    wymeditor.undoRedo.redo();
+    // There are no more things to redo.
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+});
+
+test("Toolbar buttons", function () {
+    expect(4);
+
+    var wymeditor = jQuery.wymeditors(0),
+        $buttons = wymeditor.get$Buttons(),
+        $undoButton = $buttons.filter("[name=Undo]"),
+        $redoButton = $buttons.filter("[name=Redo]");
+
+    wymeditor.html("<p>Foo</p>");
+    wymeditor.undoRedo.reset();
+    wymEqual(wymeditor, "<p>Foo</p>");
+
+    wymeditor.$body().append("<p>Bar</p>");
+    wymeditor.registerChange();
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+
     $undoButton.click();
-    wymEqual(
-        wymeditor,
-        "<ol><li>Foo</li></ol>"
-    );
-    // Use the function from now on for performance.
-    undo();
-    wymEqual(
-        wymeditor,
-        "<p>Foo</p>"
-    );
-    undo();
-    wymEqual(
-        wymeditor,
-        "<p>Foo</p>"
-    );
+    wymEqual(wymeditor, "<p>Foo</p>");
+
     $redoButton.click();
-    wymEqual(
-        wymeditor,
-        "<ol><li>Foo</li></ol>"
-    );
-    redo();
-    wymEqual(
-        wymeditor,
-        "<ul><li>Foo</li></ul>"
-    );
-    makeTextSelection(
-        wymeditor,
-        $body.find("li")[0],
-        $body.find("li")[0],
-        0,
-        3
-    );
-    wymeditor.exec('Indent');
-    wymEqual(
-        wymeditor,
-        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
-    );
-    $body.children('ul').after('<p>Bar</p>');
-    wymeditor.setCaretIn($body.children('p')[0]);
-    wymeditor.undoRedo._add();
-    undo();
-    wymEqual(
-        wymeditor,
-        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
-    );
-    selection = wymeditor.selection();
-    equal(
-        selection.toString(),
-        "Foo"
-    );
-    wymeditor.exec('OutDent');
-    undo();
-    wymEqual(
-        wymeditor,
-        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
-    );
-    // This is passed through to native `execCommand` so it should cover
-    // `Subscript`, `Superscript` and `Unlink`, as well.
-    wymeditor.exec("Italic");
-    wymEqual(
-        wymeditor,
-        "<ul><li class=\"spacer_li\"><ul><li><em>Foo</em></li></ul></li></ul>"
-    );
-    undo();
-    wymEqual(
-        wymeditor,
-        "<ul><li class=\"spacer_li\"><ul><li>Foo</li></ul></li></ul>"
-    );
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+});
+
+testUndoRedo({
+    testName: "Italic",
+    startHtml: "<p>Foo</p>",
+    prepareFunc: function (wymeditor) {
+        makeTextSelection(
+            wymeditor,
+            wymeditor.$body().children("p")[0],
+            wymeditor.$body().children("p")[0],
+            0,
+            3
+        );
+    },
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Italic");
+    },
+    expectedResultHtml: "<p><em>Foo</em></p>"
+});
+
+testUndoRedo({
+    testName: "Subscript",
+    startHtml: "<p>Foo</p>",
+    prepareFunc: function (wymeditor) {
+        makeTextSelection(
+            wymeditor,
+            wymeditor.$body().children("p")[0],
+            wymeditor.$body().children("p")[0],
+            0,
+            3
+        );
+    },
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Subscript");
+    },
+    expectedResultHtml: "<p><sub>Foo</sub></p>"
+});
+
+testUndoRedo({
+    testName: "Superscript",
+    startHtml: "<p>Foo</p>",
+    prepareFunc: function (wymeditor) {
+        makeTextSelection(
+            wymeditor,
+            wymeditor.$body().children("p")[0],
+            wymeditor.$body().children("p")[0],
+            0,
+            3
+        );
+    },
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Superscript");
+    },
+    expectedResultHtml: "<p><sup>Foo</sup></p>"
+});
+
+testUndoRedo({
+    testName: "Unlink",
+    startHtml: "<p><a>Foo</a></p>",
+    prepareFunc: function (wymeditor) {
+        makeTextSelection(
+            wymeditor,
+            wymeditor.$body().find("a")[0],
+            wymeditor.$body().find("a")[0],
+            0,
+            3
+        );
+    },
+    manipulationFunc: function (wymeditor) {
+        wymeditor.exec("Unlink");
+    },
+    expectedResultHtml: "<p>Foo</p>"
+});
+
+test("Nothing to redo after change", function () {
+    expect(4);
+
+    var wymeditor = jQuery.wymeditors(0);
+
+    wymeditor.html("<p>Foo</p>");
+    wymeditor.undoRedo.reset();
+    wymEqual(wymeditor, "<p>Foo</p>");
+
+    wymeditor.$body().append("<p>Bar</p>");
+    wymeditor.registerChange();
+    wymEqual(wymeditor, "<p>Foo</p><p>Bar</p>");
+
+    wymeditor.undoRedo.undo();
+    wymEqual(wymeditor, "<p>Foo</p>");
+
+    wymeditor.$body().append("<p>Zad</p>");
+    wymeditor.registerChange();
+    wymeditor.undoRedo.redo();
+    // Nothing was redone.
+    wymEqual(wymeditor, "<p>Foo</p><p>Zad</p>");
 });
