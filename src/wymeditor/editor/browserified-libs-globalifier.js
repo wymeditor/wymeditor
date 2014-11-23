91b6acbc2c70ed6805b3d43a25cf5eb493515474
Shahar Or <mightyiampresence@gmail.com>
2014-11-23 12:10:13 +0200

    More sane require path for Combokeys.

--- src/wymeditor/editor/browserified-libs-globalifier.js
+++ src/wymeditor/editor/browserified-libs-globalifier.js
@@ -4,4 +4,4 @@
 // See the explanation in the `Gruntfile`, in the `browserify:libs`
 // task.
 window.ObjectHistory = require("object-history/history");
-window.Combokeys = require("../../../node_modules/combokeys");
+window.Combokeys = require("combokeys");
