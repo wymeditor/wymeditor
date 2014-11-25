b05bde9aac309739613a408316246f5691deade9
Shahar Or <mightyiampresence@gmail.com>
2014-11-25 15:59:13 +0200

    exec manipulation tests use manipulationTestHelper.

--- src/test/unit/specific_feature_tests/exec_manipulations.js
+++ src/test/unit/specific_feature_tests/exec_manipulations.js
@@ -1,6 +1,7 @@
 /* jshint evil: true */
 /* global
-    testWymManipulation,
+    manipulationTestHelper,
+    test,
     prepareUnitTestModule,
     makeTextSelection,
     expect,
@@ -9,99 +10,103 @@
 "use strict";
 module("exec_manipulations", {setup: prepareUnitTestModule});
 
-testWymManipulation({
-    testName: "Bold",
-    startHtml: "<p>Foo</p>",
-    prepareFunc: function (wymeditor) {
-        var p = wymeditor.$body().children("p")[0];
-        makeTextSelection(
-            wymeditor,
-            p,
-            p,
-            0,
-            3
-        );
-    },
-    manipulationFunc: function (wymeditor) {
-        wymeditor.exec("Bold");
-    },
-    expectedResultHtml: "<p><strong>Foo</strong></p>",
-    parseHtml: true
+test("Bold", function () {
+    manipulationTestHelper({
+        startHtml: "<p>Foo</p>",
+        prepareFunc: function (wymeditor) {
+            var p = wymeditor.$body().children("p")[0];
+            makeTextSelection(
+                wymeditor,
+                p,
+                p,
+                0,
+                3
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec("Bold");
+        },
+        expectedResultHtml: "<p><strong>Foo</strong></p>",
+        parseHtml: true
+    });
 });
 
-testWymManipulation({
-    testName: "Italic",
-    startHtml: "<p>Foo</p>",
-    prepareFunc: function (wymeditor) {
-        var p = wymeditor.$body().children("p")[0];
-        makeTextSelection(
-            wymeditor,
-            p,
-            p,
-            0,
-            3
-        );
-    },
-    manipulationFunc: function (wymeditor) {
-        wymeditor.exec("Italic");
-    },
-    additionalAssertionsFunc: function (
-        // `wymeditor` seems to be unused because of the ignore, below.
-        /* jshint ignore:start */
-        wymeditor
-        /* jshint ignore:end */
-    ) {
-        expect(expect() + 1);
-        ok(
-            jQuery.inArray(
-                // JSHint does not approve of this indentation.
-                /* jshint ignore:start */
-                wymeditor.rawHtml().toLowerCase(),
-                [
-                    "<p><i>foo</i></p>",
-                    "<p><em>foo</em></p>"
-                ]
-                /* jshint ignore:end */
-            ) > -1,
-            "Either `i` or `em`."
-        );
-    }
+test("Italic", function () {
+    manipulationTestHelper({
+        startHtml: "<p>Foo</p>",
+        prepareFunc: function (wymeditor) {
+            var p = wymeditor.$body().children("p")[0];
+            makeTextSelection(
+                wymeditor,
+                p,
+                p,
+                0,
+                3
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec("Italic");
+        },
+        additionalAssertionsFunc: function (
+            // `wymeditor` seems to be unused because of the ignore, below.
+            /* jshint ignore:start */
+            wymeditor
+            /* jshint ignore:end */
+        ) {
+            expect(expect() + 1);
+            ok(
+                jQuery.inArray(
+                    // JSHint does not approve of this indentation.
+                    /* jshint ignore:start */
+                    wymeditor.rawHtml().toLowerCase(),
+                    [
+                        "<p><i>foo</i></p>",
+                        "<p><em>foo</em></p>"
+                    ]
+                    /* jshint ignore:end */
+                ) > -1,
+                "Either `i` or `em`."
+            );
+        }
+    });
 });
 
-testWymManipulation({
-    testName: "Superscript",
-    startHtml: "<p>Foo</p>",
-    prepareFunc: function (wymeditor) {
-        var p = wymeditor.$body().children("p")[0];
-        makeTextSelection(
-            wymeditor,
-            p,
-            p,
-            0,
-            3
-        );
-    },
-    manipulationFunc: function (wymeditor) {
-        wymeditor.exec("Superscript");
-    },
-    expectedResultHtml: "<p><sup>Foo</sup></p>"
+test("Superscript", function () {
+    manipulationTestHelper({
+        startHtml: "<p>Foo</p>",
+        prepareFunc: function (wymeditor) {
+            var p = wymeditor.$body().children("p")[0];
+            makeTextSelection(
+                wymeditor,
+                p,
+                p,
+                0,
+                3
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec("Superscript");
+        },
+        expectedResultHtml: "<p><sup>Foo</sup></p>"
+    });
 });
 
-testWymManipulation({
-    testName: "Subscript",
-    startHtml: "<p>Foo</p>",
-    prepareFunc: function (wymeditor) {
-        var p = wymeditor.$body().children("p")[0];
-        makeTextSelection(
-            wymeditor,
-            p,
-            p,
-            0,
-            3
-        );
-    },
-    manipulationFunc: function (wymeditor) {
-        wymeditor.exec("Subscript");
-    },
-    expectedResultHtml: "<p><sub>Foo</sub></p>"
+test("Subscript", function () {
+    manipulationTestHelper({
+        startHtml: "<p>Foo</p>",
+        prepareFunc: function (wymeditor) {
+            var p = wymeditor.$body().children("p")[0];
+            makeTextSelection(
+                wymeditor,
+                p,
+                p,
+                0,
+                3
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec("Subscript");
+        },
+        expectedResultHtml: "<p><sub>Foo</sub></p>"
+    });
 });
