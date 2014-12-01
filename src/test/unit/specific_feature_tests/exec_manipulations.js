1aca1677b80a051017dc9a52e0effa99c1c38e18
Shahar Or <mightyiampresence@gmail.com>
2014-12-02 01:42:04 +0200

    Rewritten exec_manipulation tests.

--- src/test/unit/specific_feature_tests/exec_manipulations.js
+++ src/test/unit/specific_feature_tests/exec_manipulations.js
@@ -4,13 +4,50 @@
     test,
     prepareUnitTestModule,
     makeTextSelection,
-    expect,
-    ok
+    SKIP_THIS_TEST
 */
 "use strict";
-module("exec_manipulations", {setup: prepareUnitTestModule});
+module("exec_manipulations-toggles", {setup: prepareUnitTestModule});
 
-test("Bold", function () {
+// Different browsers create wrappers with different tagNames for the same
+// execCommand commands.
+function getBrowserTagname(command) {
+    var TAGNAMES = {
+            Bold: {
+                common: "b",
+                msie7: "strong",
+                msie8: "strong",
+                msie9: "strong",
+                msie10: "strong",
+                msie11: "strong"
+            },
+            Italic: {
+                common: "i",
+                msie7: "em",
+                msie8: "em",
+                msie9: "em",
+                msie10: "em",
+                msie11: "em"
+            },
+            Superscript: {
+                common: "sup"
+            },
+            Subscript: {
+                common: "sub"
+            }
+        },
+        browserName = jQuery.browser.name,
+        browserVersion = jQuery.browser.versionNumber,
+        tagName = TAGNAMES[command][browserName + browserVersion];
+
+    if (typeof tagName !== "string") {
+        return TAGNAMES[command].common;
+    }
+    return tagName;
+}
+
+function wrapNonWrappedSelection(command) {
+    var tagName = getBrowserTagname(command);
     manipulationTestHelper({
         startHtml: "<p>Foo</p>",
         prepareFunc: function (wymeditor) {
@@ -24,92 +61,320 @@ test("Bold", function () {
             );
         },
         manipulationFunc: function (wymeditor) {
-            wymeditor.exec("Bold");
+            wymeditor.exec(command);
         },
-        expectedResultHtml: "<p><strong>Foo</strong></p>",
-        parseHtml: true
+        expectedResultHtml: "<p><" + tagName + ">Foo</" + tagName + "></p>"
     });
+}
+
+test("Wrap non-bold selection with bold", function () {
+    wrapNonWrappedSelection(WYMeditor.EXEC_COMMANDS.BOLD);
 });
 
-test("Italic", function () {
+test("Wrap non-italic selection with italic", function () {
+    wrapNonWrappedSelection(WYMeditor.EXEC_COMMANDS.ITALIC);
+});
+
+test("Wrap non-superscript selection with superscript", function () {
+    wrapNonWrappedSelection(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+test("Wrap non-subscript selection with subscript", function () {
+    wrapNonWrappedSelection(WYMeditor.EXEC_COMMANDS.SUBSCRIPT);
+});
+
+function unwrapWrappedSelection(command) {
+    var tagName = getBrowserTagname(command);
     manipulationTestHelper({
-        startHtml: "<p>Foo</p>",
+        startHtml: "<p><" + tagName + ">Foo</" + tagName + "></p>",
         prepareFunc: function (wymeditor) {
-            var p = wymeditor.$body().children("p")[0];
+            var wrapper = wymeditor.body().childNodes[0].childNodes[0];
             makeTextSelection(
                 wymeditor,
-                p,
-                p,
+                wrapper,
+                wrapper,
                 0,
                 3
             );
         },
         manipulationFunc: function (wymeditor) {
-            wymeditor.exec("Italic");
+            wymeditor.exec(command);
         },
-        additionalAssertionsFunc: function (
-            // `wymeditor` seems to be unused because of the ignore, below.
-            /* jshint ignore:start */
-            wymeditor
-            /* jshint ignore:end */
-        ) {
-            expect(expect() + 1);
-            ok(
-                jQuery.inArray(
-                    // JSHint does not approve of this indentation.
-                    /* jshint ignore:start */
-                    wymeditor.rawHtml().toLowerCase(),
-                    [
-                        "<p><i>foo</i></p>",
-                        "<p><em>foo</em></p>"
-                    ]
-                    /* jshint ignore:end */
-                ) > -1,
-                "Either `i` or `em`."
+        expectedResultHtml: "<p>Foo</p>"
+    });
+}
+
+test("Unwrap bold entirely", function () {
+    unwrapWrappedSelection(WYMeditor.EXEC_COMMANDS.BOLD);
+});
+
+test("Unwrap italic entirely", function () {
+    unwrapWrappedSelection(WYMeditor.EXEC_COMMANDS.ITALIC);
+});
+
+test("Unwrap superscript entirely", function () {
+    unwrapWrappedSelection(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+test("Unwrap subscript entirely", function () {
+    unwrapWrappedSelection(WYMeditor.EXEC_COMMANDS.SUBSCRIPT);
+});
+
+function unwrapWrappedSelectionPartially(command) {
+    var tagName = getBrowserTagname(command);
+    manipulationTestHelper({
+        startHtml: "<p><" + tagName + ">Foo</" + tagName + "></p>",
+        prepareFunc: function (wymeditor) {
+            var wrapper = wymeditor.body().childNodes[0].childNodes[0];
+            makeTextSelection(
+                wymeditor,
+                wrapper,
+                wrapper,
+                2,
+                3
             );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec(command);
+        },
+        expectedResultHtml: "<p><" + tagName + ">Fo</" + tagName + ">o</p>"
+    });
+}
+
+test("Unwrap bold partially", function () {
+    unwrapWrappedSelectionPartially(WYMeditor.EXEC_COMMANDS.BOLD);
+});
+
+test("Unwrap italic partially", function () {
+    unwrapWrappedSelectionPartially(WYMeditor.EXEC_COMMANDS.ITALIC);
+});
+
+test("Unwrap superscript partially", function () {
+    unwrapWrappedSelectionPartially(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+test("Unwrap italic partially", function () {
+    unwrapWrappedSelectionPartially(WYMeditor.EXEC_COMMANDS.SUBSCRIPT);
+});
+
+function wrapPartiallyWrappedSelection(command, skipFunc) {
+    var tagName = getBrowserTagname(command);
+    manipulationTestHelper({
+        startHtml: "<p><" + tagName + ">Fo</" + tagName + ">o</p>",
+        prepareFunc: function (wymeditor) {
+            var p = wymeditor.body().childNodes[0],
+                wrapper = p.childNodes[0];
+            makeTextSelection(
+                wymeditor,
+                wrapper,
+                p,
+                0,
+                2
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec(command);
+        },
+        expectedResultHtml: "<p><" + tagName + ">Foo</" + tagName + "></p>",
+        skipFunc: skipFunc
+    });
+}
+
+test("Most browsers wrap partially bold selection with bold", function () {
+    wrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.BOLD,
+        function () {
+            if (jQuery.browser.name === "msie") {
+                return SKIP_THIS_TEST;
+            }
         }
-    });
+    );
 });
 
-test("Superscript", function () {
+test("Most browsers wrap partially italic selection with italic", function () {
+    wrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.ITALIC,
+        function () {
+            if (jQuery.browser.name === "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+test("Most browsers wrap partially superscript selection with " +
+     "superscript", function () {
+    wrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.SUPERSCRIPT,
+        function () {
+            if (jQuery.browser.name === "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+test("Most browsers wrap partially subscript selection with " +
+     "subscript", function () {
+    wrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.SUBSCRIPT,
+        function () {
+            if (jQuery.browser.name === "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+function unwrapPartiallyWrappedSelection(command, skipFunc) {
+    var tagName = getBrowserTagname(command);
     manipulationTestHelper({
-        startHtml: "<p>Foo</p>",
+        startHtml: "<p><" + tagName + ">Fo</" + tagName + ">o</p>",
         prepareFunc: function (wymeditor) {
-            var p = wymeditor.$body().children("p")[0];
+            var p = wymeditor.body().childNodes[0],
+                wrapper = p.childNodes[0];
             makeTextSelection(
                 wymeditor,
+                wrapper,
                 p,
-                p,
+                0,
+                2
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec(command);
+        },
+        expectedResultHtml: "<p>Foo</p>",
+        skipFunc: skipFunc
+    });
+}
+
+test("IE unwraps partially bold selection", function () {
+    unwrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.BOLD,
+        function () {
+            if (jQuery.browser.name !== "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+test("IE unwraps partially italic selection", function () {
+    unwrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.ITALIC,
+        function () {
+            if (jQuery.browser.name !== "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+test("IE unwraps partially superscript selection", function () {
+    unwrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.SUPERSCRIPT,
+        function () {
+            if (jQuery.browser.name !== "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+test("IE unwraps partially subscript selection", function () {
+    unwrapPartiallyWrappedSelection(
+        WYMeditor.EXEC_COMMANDS.SUBSCRIPT,
+        function () {
+            if (jQuery.browser.name !== "msie") {
+                return SKIP_THIS_TEST;
+            }
+        }
+    );
+});
+
+function doesntWrapAcrossRootContainers(command) {
+    var noChangeHtml = "<p>Foo</p><p>Bar</p>";
+    manipulationTestHelper({
+        startHtml: noChangeHtml,
+        prepareFunf: function (wymeditor) {
+            var body = wymeditor.body(),
+                firstP = body.childNodes[0],
+                secondP = body.childNodes[1];
+
+            makeTextSelection(
+                wymeditor,
+                firstP,
+                secondP,
+                1,
+                2
+            );
+        },
+        manipulationFunc: function (wymeditor) {
+            wymeditor.exec(command);
+        },
+        expectedResultHtml: noChangeHtml
+    });
+}
+
+test("Doesn't bold across root containers", function () {
+    doesntWrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.BOLD);
+});
+
+test("Doesn't italic across root containers", function () {
+    doesntWrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.ITALIC);
+});
+
+test("Doesn't superscript across root containers", function () {
+    doesntWrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+test("Doesn't suberscript across root containers", function () {
+    doesntWrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.SUBSCRIPT);
+});
+
+function doesntUnwrapAcrossRootContainers(command) {
+    var tagName = getBrowserTagname(command),
+        noChangeHtml = "<p><" + tagName + ">Foo</" + tagName + "></p>" +
+            "<p><" + tagName + ">Bar</" + tagName + "></p>";
+    manipulationTestHelper({
+        startHtml: noChangeHtml,
+        prepareFunc: function (wymeditor) {
+            var body = wymeditor.body(),
+                firstWrapper = body.childNodes[0].childNodes[0],
+                secondWrapper = body.childNodes[1].childNodes[0];
+            makeTextSelection(
+                wymeditor,
+                firstWrapper,
+                secondWrapper,
                 0,
                 3
             );
         },
         manipulationFunc: function (wymeditor) {
-            wymeditor.exec("Superscript");
+            wymeditor.exec(command);
         },
-        expectedResultHtml: "<p><sup>Foo</sup></p>"
+        expectedResultHtml: noChangeHtml
     });
+}
+
+test("Doesn't unwrap bold across root containers", function () {
+    doesntUnwrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.BOLD);
 });
 
-test("Subscript", function () {
-    manipulationTestHelper({
-        startHtml: "<p>Foo</p>",
-        prepareFunc: function (wymeditor) {
-            var p = wymeditor.$body().children("p")[0];
-            makeTextSelection(
-                wymeditor,
-                p,
-                p,
-                0,
-                3
-            );
-        },
-        manipulationFunc: function (wymeditor) {
-            wymeditor.exec("Subscript");
-        },
-        expectedResultHtml: "<p><sub>Foo</sub></p>"
-    });
+test("Doesn't unwrap italic across root containers", function () {
+    doesntUnwrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.ITALIC);
 });
+
+test("Doesn't unwrap superscript across root containers", function () {
+    doesntUnwrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+test("Doesn't unwrap subscript across root containers", function () {
+    doesntUnwrapAcrossRootContainers(WYMeditor.EXEC_COMMANDS.SUPERSCRIPT);
+});
+
+module("exec_manipulations", {setup: prepareUnitTestModule});
 
 test("Unlink", function () {
     manipulationTestHelper({
@@ -125,7 +390,7 @@ test("Unlink", function () {
             );
         },
         manipulationFunc: function (wymeditor) {
-            wymeditor.exec("Unlink");
+            wymeditor.exec(WYMeditor.EXEC_COMMANDS.UNLINK);
         },
         expectedResultHtml: "<p>Foo</p>"
     });
