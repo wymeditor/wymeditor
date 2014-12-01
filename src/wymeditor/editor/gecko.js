fcf62189d81623eb1186e25f75297e9253bd73da
Shahar Or <mightyiampresence@gmail.com>
2014-12-01 15:44:49 +0200

    Clearer doc events handlers binding.

--- src/wymeditor/editor/gecko.js
+++ src/wymeditor/editor/gecko.js
@@ -20,13 +20,18 @@ WYMeditor.WymClassGecko.NEEDS_CELL_FIX = parseInt(
 WYMeditor.WymClassGecko.prototype._docEventQuirks = function () {
     var wym = this;
 
-    jQuery(wym._doc).bind("keydown", wym._keydown);
-    jQuery(wym._doc).bind("keyup", wym._keyup);
-    jQuery(wym._doc).bind("click", wym._click);
+    jQuery(wym._doc).bind("keydown", function (evt) {
+        wym._keydown(evt);
+    });
+    jQuery(wym._doc).bind("keyup", function (evt) {
+        wym._keyup(evt);
+    });
+    jQuery(wym._doc).bind("click", function (evt) {
+        wym._click(evt);
+    });
     // Bind editor focus events (used to reset designmode - Gecko bug)
     jQuery(wym._doc).bind("focus", function () {
-        // Fix scope
-        wym._enableDesignModeOnIframe.call(wym);
+        wym._enableDesignModeOnIframe();
     });
 };
 
@@ -55,8 +60,7 @@ WYMeditor.WymClassGecko.prototype._exec = function (cmd, param) {
 
 //keydown handler, mainly used for keyboard shortcuts
 WYMeditor.WymClassGecko.prototype._keydown = function (evt) {
-    var doc = this,
-        wym = WYMeditor.INSTANCES[doc.title];
+    var wym = this;
 
     if (evt.ctrlKey) {
         if (evt.which === WYMeditor.KEY_CODE.B) {
@@ -76,8 +80,7 @@ WYMeditor.WymClassGecko.prototype._keydown = function (evt) {
 
 // Keyup handler, mainly used for cleanups
 WYMeditor.WymClassGecko.prototype._keyup = function (evt) {
-    var doc = this,
-        wym = WYMeditor.INSTANCES[doc.title],
+    var wym = this,
         container,
         defaultRootContainer,
         notValidRootContainers,
@@ -146,8 +149,7 @@ WYMeditor.WymClassGecko.prototype._keyup = function (evt) {
 };
 
 WYMeditor.WymClassGecko.prototype._click = function () {
-    var doc = this,
-        wym = WYMeditor.INSTANCES[doc.title],
+    var wym = this,
         container = wym.selectedContainer(),
         sel;
 
