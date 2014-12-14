5ad672af12c71c677f978df20550bb37f00c2a1c
Shahar Or <mightyiampresence@gmail.com>
2014-12-14 20:31:20 +0200

    _shouldDialogOpen

--- src/wymeditor/editor/base.js
+++ src/wymeditor/editor/base.js
@@ -652,15 +652,11 @@ WYMeditor.editor.prototype.html = function (html) {
 */
 WYMeditor.editor.prototype.exec = function (cmd) {
     var wym = this,
-        container,
         custom_run;
     switch (cmd) {
 
     case WYMeditor.EXEC_COMMANDS.CREATE_LINK:
-        container = wym.getRootContainer();
-        if (container || wym._selectedImage) {
-            wym.dialog(WYMeditor.DIALOG_LINK);
-        }
+        wym.dialog(WYMeditor.DIALOG_LINK);
         break;
 
     case WYMeditor.EXEC_COMMANDS.INSERT_IMAGE:
@@ -1720,6 +1716,77 @@ WYMeditor.editor.prototype._fixDoubleBr = function () {
 };
 
 /**
+    editor._shouldDialogOpen
+    ========================
+
+    Returns true if the provided dialog type should open.
+*/
+WYMeditor.editor.prototype._shouldDialogOpen = function (dialogType) {
+    var wym = this,
+        DIALOGS = {},
+        hasSelection,
+        selection,
+        selectedContainer;
+
+    if (typeof dialogType !== "string") {
+        throw "Expected a string";
+    }
+
+    DIALOGS[WYMeditor.DIALOG_LINK] = function () {
+        if (
+            hasSelection !== true ||
+            selection.isCollapsed === true ||
+            selectedContainer === false
+        ) {
+            return false;
+        }
+        return true;
+    };
+    DIALOGS[WYMeditor.DIALOG_IMAGE] = function () {
+        if (
+            hasSelection !== true ||
+            selection.isCollapsed !== true
+        ) {
+            return false;
+        }
+        return true;
+    };
+    DIALOGS[WYMeditor.DIALOG_TABLE] = function () {
+        if (
+            hasSelection !== true ||
+            selection.isCollapsed !== true
+        ) {
+            return false;
+        }
+        return true;
+    };
+    DIALOGS[WYMeditor.DIALOG_PASTE] = function () {
+        if (
+            hasSelection !== true ||
+            selection.isCollapsed !== true
+        ) {
+            return false;
+        }
+        return true;
+    };
+    DIALOGS[WYMeditor.EXEC_COMMANDS.PREVIEW] = function () {
+        return true;
+    };
+
+    if (DIALOGS.hasOwnProperty(dialogType) !== true) {
+        throw "No such dialog type";
+    }
+
+    hasSelection = wym.hasSelection();
+    if (hasSelection) {
+        selection = wym.selection();
+        selectedContainer = wym.selectedContainer();
+    }
+
+    return DIALOGS[dialogType]();
+};
+
+/**
     editor.dialog
     =============
 
@@ -1728,16 +1795,19 @@ WYMeditor.editor.prototype._fixDoubleBr = function () {
 WYMeditor.editor.prototype.dialog = function (dialogType, dialogFeatures, bodyHtml) {
     var wym = this,
         features = dialogFeatures || wym._options.dialogFeatures,
-        wDialog = window.open('', 'dialog', features),
+        wDialog,
         sBodyHtml,
         h = WYMeditor.Helper,
         dialogHtml,
         doc;
 
-    if (wDialog) {
-        sBodyHtml = "";
+    if (wym._shouldDialogOpen(dialogType) !== true) {
+        return false;
+    }
 
-        switch (dialogType) {
+    sBodyHtml = "";
+
+    switch (dialogType) {
 
         case (WYMeditor.DIALOG_LINK):
             sBodyHtml = wym._options.dialogLinkHtml;
@@ -1757,53 +1827,61 @@ WYMeditor.editor.prototype.dialog = function (dialogType, dialogFeatures, bodyHt
         default:
             sBodyHtml = bodyHtml;
             break;
-        }
-
-        // Construct the dialog
-        dialogHtml = wym._options.dialogHtml;
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.BASE_PATH,
-            wym._options.basePath
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.DIRECTION,
-            wym._options.direction
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.WYM_PATH,
-            wym._options.wymPath
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.JQUERY_PATH,
-            wym._options.jQueryPath
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.DIALOG_TITLE,
-            wym._encloseString(dialogType)
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.DIALOG_BODY,
-            sBodyHtml
-        );
-        dialogHtml = h.replaceAllInStr(
-            dialogHtml,
-            WYMeditor.INDEX,
-            wym._index
-        );
-
-        dialogHtml = wym.replaceStrings(dialogHtml);
-
-        doc = wDialog.document;
-        doc.write(dialogHtml);
-        doc.close();
-        return wDialog;
     }
+
+    wDialog = window.open('', 'dialog', features);
+    if (
+        typeof wDialog !== "object" ||
+        wDialog.window !== wDialog
+    ) {
+        WYMeditor.console.warn("Could not create a dialog window");
+        return false;
+    }
+
+    // Construct the dialog
+    dialogHtml = wym._options.dialogHtml;
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.BASE_PATH,
+        wym._options.basePath
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.DIRECTION,
+        wym._options.direction
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.WYM_PATH,
+        wym._options.wymPath
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.JQUERY_PATH,
+        wym._options.jQueryPath
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.DIALOG_TITLE,
+        wym._encloseString(dialogType)
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.DIALOG_BODY,
+        sBodyHtml
+    );
+    dialogHtml = h.replaceAllInStr(
+        dialogHtml,
+        WYMeditor.INDEX,
+        wym._index
+    );
+
+    dialogHtml = wym.replaceStrings(dialogHtml);
+
+    doc = wDialog.document;
+    doc.write(dialogHtml);
+    doc.close();
+    return wDialog;
 };
 
 /**
